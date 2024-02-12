ca9daa31d6b3b816883a0ea916e980a3fc883d29
evelynn <145916188+evely1nn@users.noreply.github.com>
2024-02-12 11:09:46 +0300

    Add files via upload

---
+++ masked text/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace masked_text
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskTC = new System.Windows.Forms.MaskedTextBox();
+            this.maskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.maskDTarih = new System.Windows.Forms.MaskedTextBox();
+            this.maskKart = new System.Windows.Forms.MaskedTextBox();
+            this.maskIp = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T.C No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Doğum Tarihi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kart Numarası:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(71, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "IP Adresi:";
+            // 
+            // maskTC
+            // 
+            this.maskTC.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskTC.Location = new System.Drawing.Point(155, 42);
+            this.maskTC.Mask = "00000000000";
+            this.maskTC.Name = "maskTC";
+            this.maskTC.Size = new System.Drawing.Size(159, 22);
+            this.maskTC.TabIndex = 5;
+            // 
+            // maskTelefon
+            // 
+            this.maskTelefon.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskTelefon.Location = new System.Drawing.Point(155, 74);
+            this.maskTelefon.Mask = "(999) 000-0000";
+            this.maskTelefon.Name = "maskTelefon";
+            this.maskTelefon.Size = new System.Drawing.Size(159, 22);
+            this.maskTelefon.TabIndex = 6;
+            // 
+            // maskDTarih
+            // 
+            this.maskDTarih.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskDTarih.Location = new System.Drawing.Point(155, 106);
+            this.maskDTarih.Mask = "00/00/0000";
+            this.maskDTarih.Name = "maskDTarih";
+            this.maskDTarih.Size = new System.Drawing.Size(159, 22);
+            this.maskDTarih.TabIndex = 7;
+            this.maskDTarih.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskKart
+            // 
+            this.maskKart.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskKart.Location = new System.Drawing.Point(155, 138);
+            this.maskKart.Mask = "0000-0000-0000-0000";
+            this.maskKart.Name = "maskKart";
+            this.maskKart.Size = new System.Drawing.Size(159, 22);
+            this.maskKart.TabIndex = 8;
+            // 
+            // maskIp
+            // 
+            this.maskIp.Font = new System.Drawing.Font("Poor Richard", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskIp.Location = new System.Drawing.Point(155, 170);
+            this.maskIp.Mask = "###.###.###.###";
+            this.maskIp.Name = "maskIp";
+            this.maskIp.Size = new System.Drawing.Size(159, 22);
+            this.maskIp.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(393, 271);
+            this.Controls.Add(this.maskIp);
+            this.Controls.Add(this.maskKart);
+            this.Controls.Add(this.maskDTarih);
+            this.Controls.Add(this.maskTelefon);
+            this.Controls.Add(this.maskTC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskTC;
+        private System.Windows.Forms.MaskedTextBox maskTelefon;
+        private System.Windows.Forms.MaskedTextBox maskDTarih;
+        private System.Windows.Forms.MaskedTextBox maskKart;
+        private System.Windows.Forms.MaskedTextBox maskIp;
+    }
+}
+
